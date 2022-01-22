e7cef7850f3c903def4ecbd8c80fd1f53678b200
Leosr6 <leosrosa96@gmail.com>
2022-01-22 20:55:30 -0300

    Create spreadsheet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Spreadsheet from "./components/Spreadsheet";
+import { SpreadsheetInitializer } from "./spreadsheet/types";
+
+let spreadsheet: SpreadsheetInitializer;
+
+const path = window.location.pathname;
+if (path.length > 1) {
+  const id = path.substring(1);
+  const localStorageValue = localStorage.getItem(id);
+  if (localStorageValue) {
+    try {
+      const { matrix } = JSON.parse(localStorageValue);
+      if (matrix.length > 0 && matrix[0].length > 0) {
+        spreadsheet = { id, matrix };
+      }
+    } catch {}
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return spreadsheet ? (
+    <Spreadsheet {...spreadsheet} />
+  ) : (
+    <Spreadsheet rows={100} cols={30} />
   );
 }
 
