6bd0c5336ea8a930409538b320a6c4c551ec67c3
Leosr6 <leosrosa96@gmail.com>
2022-01-26 08:08:56 -0300

    Wrap load spreadsheet logic in a function

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import React from "react";
 import Spreadsheet from "./components/Spreadsheet";
 import { SpreadsheetInitializer } from "./spreadsheet/types";
 
-let spreadsheet: SpreadsheetInitializer;
-
-const path = window.location.pathname;
-if (path.length > 1) {
-  const id = path.substring(1);
-  const localStorageValue = localStorage.getItem(id);
-  if (localStorageValue) {
-    try {
-      const { matrix } = JSON.parse(localStorageValue);
-      if (matrix.length > 0 && matrix[0].length > 0) {
-        spreadsheet = { id, matrix };
-      }
-    } catch {}
+const loadSpreadsheet = (): SpreadsheetInitializer | null => {
+  const path = window.location.pathname;
+  if (path.length > 1) {
+    const id = path.substring(1);
+    const localStorageValue = localStorage.getItem(id);
+    if (localStorageValue) {
+      try {
+        const { matrix } = JSON.parse(localStorageValue);
+        if (matrix.length > 0 && matrix[0].length > 0) {
+          return { id, matrix };
+        }
+      } catch {}
+    }
   }
-}
+  return null;
+};
 
 function App() {
+  const spreadsheet = loadSpreadsheet();
+
   return spreadsheet ? (
     <Spreadsheet {...spreadsheet} />
   ) : (
